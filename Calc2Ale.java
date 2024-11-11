946f5fe07fc4d75a5454608ae6484abe0251b22f
AnnieC <anncomba@gmail.com>
2024-11-11 20:05:22 -0300

    prueba

--- Calc2Ale.java
+++ Calc2Ale.java
@@ -6,6 +6,7 @@ class Calc2Ale {
         Scanner sc = new Scanner(System.in);
         int resultado;
         int opcion;
+        
         do {
             System.out.print("Ingresa el primer operando: ");
             int operando1 = sc.nextInt();
